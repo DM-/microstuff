6088682b8b71704119f813b79b0cb5cf80e8bf76
DM <kingcocomango@gmail.com>
2015-11-02 22:03:30 +0100

    forgot to add a delay , so it was looping so fast I saw it as 50%

---
+++ Makefile
@@ -0,0 +1,6 @@
+upload%: %.hex
+	avrdude -v -P /dev/ttyUSB0 -p attiny45 -c avrisp -b 19200 -U flash:w:$*.hex
+%.elf: %.c
+	avr-gcc -mmcu=attiny45 -Wall -Os -o $@ $<
+%.hex: %.elf
+	avr-objcopy -R .eeprom -O ihex $< $@
--- fade.c
+++ fade.c
@@ -5,11 +5,14 @@
 
 int main(void){
 	DDRB |= _BV(0); // set pin 0 as output
-	TCCR0A |= (_BV(COM0A1)|_BV(WGM00)|_BV(WGM01)); // set the output pin, and set fast pwm
-	OCR0A = 0x7D // set it to half duty cycle
-	// OCR0A = 0x00 // set it to 0 duty cycle
-	// OCR0A = 0xff // set it to 100 duty cycle
+	TCCR0A |= (_BV(COM0A1)|_BV(WGM00)|_BV(WGM00)); // set the output pin, and set fast pwm
+	TCCR0B |= _BV(CS00);
+	OCR0A = 0x7D; // set it to half duty cycle
+	//OCR0A = 0x00; // set it to 0 duty cycle
+	//OCR0A = 0xff; // set it to 100 duty cycle
 	while(1){
+		unsigned char temp=OCR0A;
+		OCR0A=temp+10;
+		_delay_ms(100);
 		}
-	}
 }
