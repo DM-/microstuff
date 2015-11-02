9b6ba0e15b9d871f7edd7809ce59196acf748d7f
DM <kingcocomango@gmail.com>
2015-11-02 23:09:18 +0100

    button speeds up movement

--- fade.c
+++ fade.c
@@ -5,18 +5,24 @@
 
 int main(void){
 	DDRB |= _BV(0); // set pin 0 as output
+	DDRB &= ~_BV(4);
+	PORTB |= _BV(4); //set input pin pullup
 	TCCR0A |= (_BV(COM0A1)|_BV(WGM00)|_BV(WGM00)); // set the output pin, and set fast pwm
 	TCCR0B |= _BV(CS00);
 	OCR0A = 0x7D; // set it to half duty cycle
 	//OCR0A = 0x00; // set it to 0 duty cycle
 	//OCR0A = 0xff; // set it to 100 duty cycle
-	volatile int changeInRegister = 1;
+	int changeInRegister = 1;
 	while(1){
 		if ((OCR0A==0xff|OCR0A==0x00))
 		{
 			changeInRegister = changeInRegister * -1;
 		}
 		OCR0A = OCR0A + changeInRegister;
-		_delay_ms(10);
+		if (bit_is_set(PINB,4))
+		{
+			_delay_ms(9);
+		}
+		_delay_ms(1);
 		}
 }
