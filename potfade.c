b4286c395ffd954be7e79bb087acf84ae33b9b88
DM <kingcocomango@gmail.com>
2015-11-09 21:50:59 +0100

    Changed pwm to 8bit from 10bit, to match the ADCH read byte

--- potfade.c
+++ potfade.c
@@ -5,7 +5,7 @@
 
 int main(void){
 	DDRB |= _BV(1); // set pin 0 as output
-	TCCR1A |= (_BV(COM1A1)|_BV(WGM10)|_BV(WGM11)); // set the output pin, and set fast pwm, and select output pin
+	TCCR1A |= (_BV(COM1A1)|_BV(WGM10)); // set the output pin, and set fast pwm(8bit), and select output pin
 	TCCR1B |= _BV(CS10)|_BV(WGM12); //continue setting fast pwm, and set clock source = clk.io
 	ADMUX |= _BV(REFS0)|_BV(ADLAR); // set the refrence to avcc, and left adjust the results so we can just read ADCH
 	ADCSRA |= _BV(ADEN)|_BV(ADSC)|_BV(ADFR)|_BV(ADPS0)|_BV(ADPS1)|_BV(ADPS2); 
