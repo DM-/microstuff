949ebef2d21f47c6e547ba2e564ce7449a3ce22c
DM <kingcocomango@gmail.com>
2015-11-09 21:16:36 +0100

    made fade work with atmega8

--- fade.c
+++ fade.c
@@ -4,20 +4,20 @@
 #include <util/delay.h>
 
 int main(void){
-	DDRB |= _BV(0); // set pin 0 as output
+	DDRB |= _BV(1); // set pin 0 as output
 	PORTB |= _BV(4); //set  input pin pullup
-	TCCR0A |= (_BV(COM0A1)|_BV(WGM00)|_BV(WGM01)); // set the output pin, and set fast pwm
-	TCCR0B |= _BV(CS00);
-	OCR0A = 0x7D; // set it to half duty cycle
+	TCCR1A |= (_BV(COM1A1)|_BV(WGM10)|_BV(WGM11)); // set the output pin, and set fast pwm
+	TCCR1B |= _BV(CS10)|_BV(WGM12);
+	OCR1A = 0x7D; // set it to half duty cycle
 	//OCR0A = 0x00; // set it to 0 duty cycle
 	//OCR0A = 0xff; // set it to 100 duty cycle
 	int changeInRegister = 1;
 	while(1){
-		if ((OCR0A==0xff)|(OCR0A==0x00))
+		if ((OCR1A>0xff)|(OCR1A==0x00))
 		{
 			changeInRegister = changeInRegister * -1;
 		}
-		OCR0A = OCR0A + changeInRegister;
+		OCR1A = OCR1A + changeInRegister;
 		if (bit_is_set(PINB,4))
 		{
 			_delay_ms(9);
