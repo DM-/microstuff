b7dd73228a423c2b382a9496ae2610e7a5045ff1
DM <kingcocomango@gmail.com>
2015-11-12 22:13:30 +0100

    started using define for config

--- simblink.c
+++ simblink.c
@@ -2,11 +2,12 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#define BLINKPORT 1
 
 int main(void){
-	DDRB |= _BV(0); // set pin 3 as output
+	DDRB |= _BV(BLINKPORT); // set pin 3 as output
 	while(1){
-		PORTB ^= _BV(0);
+		PORTB ^= _BV(BLINKPORT);
 		_delay_ms(100);
 	}
 }
