8086d16e046c1a8b0918733da562cb5899c393db
DM <kingcocomango@gmail.com>
2015-11-13 00:19:12 +0100

    added 1 to blik str

--- fade.c
+++ fade.c
@@ -2,12 +2,13 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include "INCLUDEHEADER.h"
 
 int main(void){
 	DDRB |= _BV(1); // set pin 0 as output
 	TCCR1A |= (_BV(COM1A1)|_BV(WGM10)); // set the output pin, and set fast pwm
 	TCCR1B |= _BV(CS10)|_BV(WGM12); // and set clock 
-	OCR1A = 0x7D; // set it to half duty cycle
+	OCR1A = 0x80; // set it to half duty cycle
 	//OCR0A = 0x00; // set it to 0 duty cycle
 	//OCR0A = 0xff; // set it to 100 duty cycle
 	int changeInRegister = 1;
