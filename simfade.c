6ee93cf67c1c4892207f15995da7ea806774737f
DM <kingcocomango@gmail.com>
2015-11-13 11:59:28 +0100

    MORE DEFINE CONFIGS

--- simfade.c
+++ simfade.c
@@ -1,9 +1,10 @@
 #include <avr/io.h>
-#define STRENGTH 0x80
+#define STRENGTH 0x00
+#define FADEPORT PB1
 
 
 int main(void){
-	DDRB |= _BV(1); // set pin 0 as output
+	DDRB |= _BV(FADEPORT); // set pin 0 as output
 	TCCR1A |= _BV(COM1A1)|_BV(WGM10); // set the output pin, and set fast pwm(8bit), and select output pin
 	TCCR1B |= _BV(CS10)|_BV(WGM12); //continue setting fast pwm, and set clock source = clk.io
 	OCR1A = STRENGTH;
