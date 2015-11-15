910f6d8baf0f611ab98bd69701e38977a576e62c
DM <kingcocomango@gmail.com>
2015-11-15 01:02:11 +0100

    Working on generic assembly code

--- sfade.asm
+++ sfade.asm
@@ -1,15 +1,19 @@
+#define __SFR_OFFSET 0
+
+#include "avr/io.h"
+
 .section	.text
 .global	main
 main:
-.L__stack_usage = 0
-	sbi 0x17,1
-	in r24,0x2f
-	ori r24,-127
-	out 0x2f,r24
-	in r24,0x2e
-	ori r24,9
-	out 0x2e,r24
-	ldi r24,1
-	out 0x2a,r24
+.L1:
+	sbi _SFR_IO_ADDR(DDRB) , PB1
+	in r24 , _SFR_IO_ADDR(TCCR1A)
+	ori r24 , -127
+	out _SFR_IO_ADDR(TCCR1A) , r24
+	in r24 , _SFR_IO_ADDR(TCCR1B)
+	ori r24 , 9
+	out _SFR_IO_ADDR(TCCR1B) , r24
+	ldi r24 , 1
+	out _SFR_IO_ADDR(OCR1A) , r24
 .L2:
 	rjmp .L2
