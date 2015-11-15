08882346c4a0acc7ca018d8ec16ba0210ff4f210
DM <kingcocomango@gmail.com>
2015-11-15 14:42:45 +0100

    Checking out that the lib works before putting in more work. Also doing something slightly fancy.

--- lcd.c
+++ lcd.c
@@ -1,5 +1,7 @@
 #define REVERSED 1
+#define F_CPU 8000000
 #include "lcdlib.c"
+#include <util/delay.h>
 
 int main(void)
 {
@@ -8,8 +10,30 @@ int main(void)
 	SendCharacter('e');
 	SendCharacter('s');
 	SendCharacter('t');
-	SendCommand(0xC8);
-	SendString("testing");
+	SendCharacter('i');
+	SendCharacter('n');
+	SendCharacter('g');
+	SendCharacter('1');
+	SendCharacter('2');
+	SendCharacter('3');
+	SendCommand(0xA5);
+	SendCharacter('A');
+	SendCharacter('L');
+	SendCharacter('P');
+	SendCommand(0xC0);
+	SendCharacter('t');
+	SendCharacter('e');
+	SendCharacter('s');
+	SendCharacter('t');
+	SendCharacter('i');
+	SendCharacter('n');
+	SendCharacter('g');
+	SendCharacter('4');
+	SendCharacter('5');
+	SendCharacter('6');
+	SendCommand(0xE6);
+	SendCharacter('H');
+	SendCharacter('A');
 	while(1){
 
 	}
