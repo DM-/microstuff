db2e9e956564981116b7ed59d6d1036f6c0465a2
DM <kingcocomango@gmail.com>
2015-11-15 13:54:36 +0100

    Working more on the header

--- lcdlib.c
+++ lcdlib.c
@@ -1,28 +1,15 @@
-#define F_CPU 8000000
-
 #include "RBLT.h"
-#include <avr/io.h>
-#include <util/delay.h>
 
-
-
-#define CONTROLPORT 		PORTB  	// The port on which the control pins are
-#define CONTROLPORTDIR	DDRB	// The control register for the control pins
-#define ENABLE 			0 		// The pin for the enable signal on the control port //1= lcd reads stuff, falling edge = lcd does stuff	
-#define REGISTERSELECT 	7		// The pin for the RS signal on the control port // 1=character, 0=command
-#define READWRITE 		6		// The pin for the RW signal on the control port // 1= read, 0=write
-#define DATAPORT			PORTD	// The port on which the data pins are written to
-#define DATAPIN			PIND	// The input register for the data pins
-#define DATAPORTDIR		DDRD	// The control register for the data pins
-#define REVERSED			1		// Set this to one if the data port between uC and LCD are reversed. 0 Otherwise.
-								// Reversed uses a 300 byte lookuptable and  few cycles per character. Not recommended.
-								// You can set Reversed to 0 and manually invoke SendCharacter(RBLT(OUTPUT)) to have
-								// The preprocessor optimize it out (if its static). Wont work on runtime generated output.
+##ifndef REVERSED
+#define REVERSED 0  // Set this to 1 here, on as a config define in your file to get stuff right
+#endif
 
 #if REVERSED
 #define OUT RBLT
+#define InitLcd InitLcdASMR
 #else
 #define OUT(X) X
+#define InitLcd InitLcdASM
 #endif
 
 #define SHIFTCURSORRIGHT	SendCommand(0x14)
@@ -35,8 +22,7 @@
 
 static void SendCommandASM(unsigned char);
 static void SendCharacterASM(unsigned char);
-static void InitLcdASM(void);
-static void InitLcdASMR(void);
+static void InitLcd(void); // Actually is replaced by asm version by preprocessor.
 void SendStringASM(char *); // There is no reversed option for sendstring
 #define SendString 			SendStringASM
 
