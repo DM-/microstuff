5c2a1f676ac0110d81c34b90d9e2095e76b33937
DM <kingcocomango@gmail.com>
2015-11-15 13:50:14 +0100

    working on that .h wrapper

--- lcdlib.c
+++ lcdlib.c
@@ -1,20 +1,20 @@
-#define F_CPU 8000000 //remove this after testing
+#define F_CPU 8000000
 
+#include "RBLT.h"
 #include <avr/io.h>
 #include <util/delay.h>
-#include "RBLT.h"
 
 
 
-#define CONTROLPORT 	PORTB  	// The port on which the control pins are
+#define CONTROLPORT 		PORTB  	// The port on which the control pins are
 #define CONTROLPORTDIR	DDRB	// The control register for the control pins
 #define ENABLE 			0 		// The pin for the enable signal on the control port //1= lcd reads stuff, falling edge = lcd does stuff	
 #define REGISTERSELECT 	7		// The pin for the RS signal on the control port // 1=character, 0=command
 #define READWRITE 		6		// The pin for the RW signal on the control port // 1= read, 0=write
-#define DATAPORT		PORTD	// The port on which the data pins are written to
+#define DATAPORT			PORTD	// The port on which the data pins are written to
 #define DATAPIN			PIND	// The input register for the data pins
 #define DATAPORTDIR		DDRD	// The control register for the data pins
-#define REVERSED		1		// Set this to one if the data port between uC and LCD are reversed. 0 Otherwise.
+#define REVERSED			1		// Set this to one if the data port between uC and LCD are reversed. 0 Otherwise.
 								// Reversed uses a 300 byte lookuptable and  few cycles per character. Not recommended.
 								// You can set Reversed to 0 and manually invoke SendCharacter(RBLT(OUTPUT)) to have
 								// The preprocessor optimize it out (if its static). Wont work on runtime generated output.
@@ -25,41 +25,27 @@
 #define OUT(X) X
 #endif
 
-#define SHIFTCURSORRIGHT	SendCommand(OUT(0x14))
-#define SHIFTCURSORLEFT		SendCommand(OUT(0x10))
-#define SHIFTSCREENRIGHT	SendCommand(OUT(0x1C))
-#define SHIFTSCREENLEFT		SendCommand(OUT(0x18))
-#define CLEARDISPLAY		SendCommand(OUT(0x01))
-#define	HOMEDISPLAY			SendCommand(OUT(0x02))
-
+#define SHIFTCURSORRIGHT	SendCommand(0x14)
+#define SHIFTCURSORLEFT		SendCommand(0x10)
+#define SHIFTSCREENRIGHT	SendCommand(0x1C)
+#define SHIFTSCREENLEFT		SendCommand(0x18)
+#define CLEARDISPLAY		SendCommand(0x01)
+#define	HOMEDISPLAY			SendCommand(0x02)
 ; // just here to fix a syntax highlight bug in sublime
+
+static void SendCommandASM(unsigned char);
+static void SendCharacterASM(unsigned char);
+static void InitLcdASM(void);
+static void InitLcdASMR(void);
+void SendStringASM(char *); // There is no reversed option for sendstring
+#define SendString 			SendStringASM
+
+
+
 static void SendCommand(unsigned char command){
 	SendCommandASM(OUT(command));
 }
 
 static void SendCharacter(unsigned char character){
 	SendCharacterASM(OUT(character));
-}
-
-static void SendString(char *String){
-	while(*String){
-		SendCharacterASM(*String++);
-	}
-}
-
-volatile int XD = 0x38;
-void InitLcd(void){
-	CONTROLPORTDIR |= _BV(ENABLE);
-	CONTROLPORTDIR |= _BV(REGISTERSELECT);
-	CONTROLPORTDIR |= _BV(READWRITE);
-	_delay_ms(40);
-	SendCommand(0x38); //Function set, for reset
-	_delay_us(40);
-	SendCommand(0x38); //Function set, for real
-	_delay_us(40);
-	SendCommand(0x0E); //Display, cursor and blink off
-	_delay_us(40);
-	SendCommand(0x01); //Clear the display
-	_delay_ms(4);
-	SendCommandASM(0x06); //Entry mode set to increment, no shift
 }
--- lcdlibasm.asm
+++ lcdlibasm.asm
@@ -60,7 +60,46 @@ SendStringASM:
 	.size SendStringASM, .-SendStringASM
 .global	InitLcd
 	.type	InitLcd, @function
-InitLcd:
+InitLcdASM:
+	sbi 0x17,0
+	sbi 0x17,7
+	sbi 0x17,6
+	ldi r18,lo8(63999)
+	ldi r24,hi8(63999)
+	ldi r25,hlo8(63999)
+	1: subi r18,1
+	sbci r24,0
+	sbci r25,0
+	brne 1b
+	rjmp .
+	nop
+	ldi r24,lo8(0x38)
+	rcall SendCommandASM
+	ldi r18,lo8(40)
+	1: subi r18,1
+	brne 1b
+	ldi r24,lo8(0x38)
+	rcall SendCommandASM
+	ldi r18,lo8(40)
+	1: subi r18,1
+	brne 1b
+	ldi r24,lo8(0x0E)
+	rcall SendCommandASM
+	ldi r18,lo8(40)
+	1: subi r18,1
+	brne 1b
+	ldi r24,lo8(0x01)
+	rcall SendCommandASM
+	ldi r24,lo8(2000)
+	ldi r25,hi8(2000)
+	1: sbiw r24,1
+	brne 1b
+	ldi r24,lo8(0x06)
+	rcall SendCommandASM
+	ret
+	.size	InitLcd, .-InitLcd
+
+InitLcdASMR: //reversed InitLcdASM
 	sbi 0x17,0
 	sbi 0x17,7
 	sbi 0x17,6
